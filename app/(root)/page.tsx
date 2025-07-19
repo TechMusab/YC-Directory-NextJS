0b6b1a988179e4f69521acf1984aff75f6b3aca1
Musab <joiya4636@gmail.com>
2025-07-19 15:28:07 -0700

    Hero Section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,22 @@
-import React from 'react'
-
-export default function Page() {
-
-  
-
+import React from "react";
+import SearchForm from "../../components/SearchForm";
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const {query} = await searchParams;
   return (
     <>
-     <h1 className='text-2xl'>HOME</h1>
+      <section className="pink-container pattern">
+        <h1 className="heading">
+          PITCH YOUR START UP AND CONNECT WITH ENTERPRENEURS
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches and Get Noticed on Virtual Competitions
+        </p>
+        <SearchForm query={query} />
+      </section>
     </>
-  )
+  );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,7 @@ export default async function Navbar() {
     const session = await auth();
 
     return (
-        <header className=" text-black px-5 py-3 bg-white shadow-sm font-work-sans">
+        <header className="px-5 py-3 shadow-sm font-work-sans bg-white">
             <nav className="flex justify-between items-center">
                 <Link href="/">
                     <Image src="/logo.png" alt="logo" width={144} height={30} />
